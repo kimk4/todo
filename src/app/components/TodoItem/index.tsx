221d9c246afac0ace292c037b6189fce60428126
kim <kimk40000@gmail.com>
2024-04-17 14:36:16 +0900

    delete-button"

--- src/app/components/TodoItem/index.tsx
+++ src/app/components/TodoItem/index.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import Block from '../Block';
 import Checkbox from '../Checkbox';
+import CircleButton from '../Buatton/CircleButton';
 
 const Box = styled.div`
   display: flex;
@@ -29,6 +30,22 @@ export default function TodoItem({ todo }: { todo: ITodoItem }) {
       <Checkbox checked={todo.completed} />
       <Block marginLeft="10px" />
       <TodoContent checked={false}>{todo.content}</TodoContent>
+      <CircleButton
+        className="delete-button"
+        onClick={() => {}}
+        Icon={() => (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#000000"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z" />
+          </svg>
+        )}
+      />
     </Box>
   );
 }
